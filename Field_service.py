ff4fa0f9d06b3603fa83ed85044ca32fb6633d6e
othnielamos <othnielamos9@gmail.com>
2025-12-17 00:51:17 +0100

    Values in dictionary can now be added to csv file and vice versa,App updates as well

--- Field_service.py
+++ Field_service.py
@@ -1,81 +1,83 @@
 import time
 import functions
-from functions import update_dict_temp
+import pandas as pd
 
-field_service = {"Date":["05/05/2009","05/06/2007"],"Hours":["30","40"],
-                 "Student":["Michael","Marcus"],"Notes":["Listened well","Barely Listened"]}
+DATA_FILE = "data_values.csv"
+field_service = functions.csv_to_dict(DATA_FILE)
 
 print("This is the field service app:")
 print("Use this to keep track of your clients")
-choice = input("Edit (1), View (2), Delete(3), Add(4): ")
+choice = ""
 
-if choice == "1":
-    functions.display(field_service)
-    date_edit = input("Please enter the date you would like to edit:")
-    #Searches for the date in the larger dictionary
-    temp_field_service,index = functions.search(field_service,date_edit)
+while choice != "5":
+    choice = input("Edit (1), View (2), Delete(3), Add(4), Quit(5): ")
+    if choice == "1":
+        functions.display(field_service)
+        date_edit = input("Please enter the date you would like to edit:")
+        #Searches for the date in the larger dictionary
+        temp_field_service,index = functions.search(field_service,date_edit)
 
-    functions.display(temp_field_service)
-    done = True
+        functions.display(temp_field_service)
+        done = True
 
-    while done:
-        print("Date (1), Hours (2), Student (3), Notes (4)")
-        var_to_edit = int(input("Please select the number you wish to edit:"))
+        while done:
+            print("Date (1), Hours (2), Student (3), Notes (4)")
+            var_to_edit = int(input("Please select the number you wish to edit:"))
 
-        #find the specific key value correlating to the choice the user selected in var_to_edit
-        word = list(temp_field_service.keys())[var_to_edit-1]
+            #find the specific key value correlating to the choice the user selected in var_to_edit
+            word = list(temp_field_service.keys())[var_to_edit-1]
 
-        change = input(f"Change {str(temp_field_service[word])[2:-2]} to...")
-        temp_field_service[word][0] = str(change)
-        done = input("Would you like to edit this date again (Y/N):")
-        if done == "N":
-            done = False
-
-
-    #Updates the field service dict
-    functions.update_dict_temp(field_service,temp_field_service,index)
+            change = input(f"Change {str(temp_field_service[word])[2:-2]} to...")
+            temp_field_service[word][0] = str(change)
+            done = input("Would you like to edit this date again (Y/N):")
+            if done == "N":
+                done = False
 
 
-    print(field_service)
-
+        #Updates the field service dict
+        functions.update_dict_temp(field_service,temp_field_service,index)
 
 
 
-elif choice == "2":
-    functions.display(field_service)
 
-elif choice == "3":
-    date_edit = input("Please enter the date you would like to delete:")
-    temp_field_service,index = functions.search(field_service,date_edit)
-    functions.display(temp_field_service)
-    choice = input("Please confirm the deletion protocol (Y/N):")
+    elif choice == "2":
+        functions.display(field_service)
 
-    if choice == "Y":
-        for key in field_service.keys():
-            del field_service[key][index]
+    elif choice == "3":
+        date_edit = input("Please enter the date you would like to delete:")
+        temp_field_service,index = functions.search(field_service,date_edit)
+        functions.display(temp_field_service)
+        choice = input("Please confirm the deletion protocol (Y/N):")
+
+        if choice == "Y":
+            for key in field_service.keys():
+                del field_service[key][index]
 
 
-    print("Deletion was successful")
+        print("Deletion was successful")
 
-elif choice == "4":
-    temp_field = []
-    current_date = input("Please input date or type 'Y' to input current date:")
-    if current_date == "Y":
-        current_date = time.strftime("%d/%m/%Y")
+    elif choice == "4":
+        temp_field = []
+        current_date = input("Please input date or type 'Y' to input current date:")
+        if current_date == "Y":
+            current_date = time.strftime("%d/%m/%Y")
 
-    temp_field.append(current_date)
+        temp_field.append(current_date)
 
-    questions = ["Number of Hours:","Student Name:","Notes:"]
+        questions = ["Number of Hours:","Student Name:","Notes:"]
 
-    for question in questions:
-        add_it = input(f"{question}")
-        temp_field.append(add_it)
+        for question in questions:
+            add_it = input(f"{question}")
+            temp_field.append(add_it)
 
-    #Adds the values to the field_service dictionary
-    for index, key in enumerate(field_service.keys()):
-        field_service[key].append(temp_field[index])
+        #Adds the values to the field_service dictionary
+        for index, key in enumerate(field_service.keys()):
+            field_service[key].append(temp_field[index])
 
-    print(field_service)
+
+    functions.dict_to_csv(field_service,DATA_FILE)
+
+
 
 
 
--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
+import pandas as pd
+from pandas import DataFrame
+
+
 def display(database):
+    '''Displays the values of a dictionary in a field service format'''
     for index, day in enumerate(database["Date"]):
         print(f"Day:{database["Date"][index]}")
         print(f"Number of hours:{database["Hours"][index]}")
@@ -7,6 +12,7 @@ def display(database):
         print("\n")
 
 def search(database,date_edit):
+    '''Searches a dictionary based on the date given'''
     temp_field_service ={}
     for index, date in enumerate(database["Date"]):
         if date == date_edit:
@@ -16,7 +22,52 @@ def search(database,date_edit):
     return temp_field_service,index
 
 def update_dict_temp(database,temp_database,index):
+    '''Updates a dictionary based on the values of another dictionary'''
     for key in database.keys():
         add_to = str(temp_database[key])[2:-2]
         database[key][index] = add_to
+
+def csv_to_dict(datafile):
+    """Converts a csv file to a dictionary"""
+    df = pd.read_csv(datafile)
+    database = df.to_dict(orient="list")
+    return database
+
+def dict_to_csv(database,datafile):
+    """Converts a dictionary to a csv file (Used after altering the dictionary)"""
+    df = pd.DataFrame(database)
+    df.to_csv(datafile, index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
