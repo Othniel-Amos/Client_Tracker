27da61304e86aad69c23ea2848b854a72fa5b36e
othnielamos <othnielamos9@gmail.com>
2026-01-06 14:27:31 +0100

    Initial commit for README.md

--- functions.py
+++ functions.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import os
-from pandas import DataFrame
 from datetime import datetime
 
 
 def display(database):
-    '''Displays the values of a dictionary in a field service format'''
+    """Displays the values of a dictionary in a readable format"""
     index = 0
     for day in database["Date"]:
         print(f"Day:{database["Date"][index]}")
@@ -16,7 +15,11 @@ def display(database):
         index += 1
 
 def search(database,date_edit,dict_key="Date",multiple=False):
-    '''Searches a dictionary based on the date given'''
+    """Searches a dictionary based on the date given.
+    It can also search by using other parameters of the dictionary:
+    'Hours', 'Students' & 'Notes'.
+    With multiple set to false it will return only the last instance of the specified searchable
+    (this isn't a problem for dates as dates are expected to be unique and never repeat)."""
     try:
         temp_field_service ={}
         dict_list = []
@@ -51,30 +54,33 @@ def search(database,date_edit,dict_key="Date",multiple=False):
             return temp_field_service, index,found
 
 def update_dict_temp(database,temp_database,index):
-    '''Updates a dictionary based on the values of another dictionary'''
+    """Updates a dictionary based on the values of another dictionary
+    (It will add a temporary dictionary to the main dictionary)"""
     for key in database.keys():
         add_to = str(temp_database[key]).strip("''[]")
         database[key][index] = add_to
 
 def csv_to_dict(datafile):
-    """Converts a csv file to a dictionary"""
+    """Converts a csv file to a dictionary
+    (This'll be a nested dictionary with keys and lists as values)"""
     df = pd.read_csv(datafile)
     database = df.to_dict(orient="list")
     return database
 
 def dict_to_csv(database,datafile):
-    """Converts a dictionary to a csv file (Used after altering the dictionary)"""
+    """Converts a dictionary to a csv file (used after altering the dictionary)"""
     df = pd.DataFrame(database)
     df.to_csv(datafile, index=False)
 
 def check_if_dict_empty(database):
+    """Checks if a dictionary is empty and returns true or false"""
     if any(database.values()):
         return True
     else:
         return False
 
 def validate_dict(database):
-    '''Validates if the user is using the right type of values'''
+    """Validates if the user is using the right type of values"""
     error_message = ""
     valid = True
 
@@ -116,6 +122,7 @@ def validate_dict(database):
     return valid,error_message
 
 def csv_exists(datafile):
+    """Check if a csv file exist and if not it is created"""
     if os.path.exists(datafile):
         pass
     else:
---
+++ main.py
@@ -0,0 +1,226 @@
+import time
+import functions
+import copy
+
+DATA_FILE = "data_values.csv"
+functions.csv_exists(DATA_FILE)
+field_service = functions.csv_to_dict(DATA_FILE)
+
+def main_edit():
+    functions.display(field_service)
+    date_edit = input("Please enter the date you would like to edit:")
+    # Searches for the date in the larger dictionary
+    try:
+        temp_field_service, index, found = functions.search(field_service, date_edit)
+    except Exception as exception:
+        found = False
+    else:
+        if found:
+            functions.display(temp_field_service)
+
+    while found:
+        print("Date (1), Hours (2), Student (3), Notes (4)")
+        try:
+            var_to_edit = int(input("Please select the number you wish to edit:").strip())
+        except:
+            print("Please type an integer value")
+        else:
+            # find the specific key value correlating to the choice the user selected in var_to_edit
+            word = list(temp_field_service.keys())[var_to_edit - 1]
+
+            change = input(f"Change {str(temp_field_service[word]).strip("''[]")} to...")
+
+            temp_field_service_2 = copy.deepcopy(temp_field_service)
+            temp_field_service_2[word][0] = str(change)
+
+            valid, error_message = functions.validate_dict(temp_field_service_2)
+
+            if valid:
+                temp_field_service = temp_field_service_2
+                functions.update_dict_temp(field_service, temp_field_service, index)
+            else:
+                print(f"ERROR:{error_message} in {change}")
+                if var_to_edit == 1:
+                    print("Valid date format:DD/MM/YYYY")
+                else:
+                    print("Hours must be in a numerical value")
+                    print("Example:34")
+
+            done = input("Would you like to edit this date again (Y/N):")
+            if done == "N":
+                found = False
+
+def main_delete():
+    date_edit = input("Please enter the date you would like to delete:")
+    try:
+        temp_field_service, index, found = functions.search(field_service, date_edit)
+    except TypeError:
+        print(f"{date_edit} does not exist in the records\n")
+    else:
+        functions.display(temp_field_service)
+        choice_delete = input("Please confirm the deletion protocol (Y/N):")
+
+        if choice_delete == "Y":
+            for key in field_service.keys():
+                del field_service[key][index]
+        print("Deletion was successful")
+
+def main_add():
+    add_again = True
+
+    while add_again:
+        temp_field = []
+        temp_field_dict = {"Date": [], "Hours": [], "Student": [], "Notes": []}
+        current_date = input("Please input date or type 'Y' to input current date:")
+        if current_date == "Y":
+            current_date = time.strftime("%d/%m/%Y")
+
+        temp_field.append(current_date)
+
+        questions = ["Number of Hours:", "Student Name:", "Notes:"]
+
+        for question in questions:
+            add_it = input(f"{question}")
+            temp_field.append(add_it)
+
+        for index, key in enumerate(field_service.keys()):
+            temp_field_dict[key] = temp_field[index]
+
+        valid, error_message = functions.validate_dict(temp_field_dict)
+        if valid:
+            # Adds the values to the field_service dictionary
+            for index, key in enumerate(field_service.keys()):
+                field_service[key].append(temp_field[index])
+        else:
+            print(f"{error_message}")
+
+        add_again = input("Would you like to add again (Y/N):").strip().upper()
+
+        if add_again == "Y":
+            add_again = True
+        else:
+            add_again = False
+
+def main_search():
+    # Searches the dictionary for all values
+    print("Date (1), Hours (2), Student (3), Notes (4)")
+    var_to_search = ""
+
+    while type(var_to_search) != int:
+        try:
+            var_to_search = int(input("Please select the number you wish to search by:").strip())
+        except:
+            print("Please enter an integer value")
+
+    while var_to_search not in [1, 2, 3, 4]:
+        print("ERROR:Invalid variable")
+        var_to_search = int(input("Please select the number you wish to search by:"))
+
+    search_again = True
+
+    while search_again:
+        search_value = input("Please enter the searchable value:").strip()
+        if var_to_search == 1:
+            temp_field_service, index, found = functions.search(field_service, search_value, "Date", False)
+        elif var_to_search == 2:
+            temp_field_service, index, found = functions.search(field_service, search_value, "Hours", True)
+        elif var_to_search == 3:
+            temp_field_service, index, found = functions.search(field_service, search_value, "Student", True)
+        else:
+            temp_field_service, index, found = functions.search(field_service, search_value, "Notes", True)
+
+        # Checks if to display the field or not
+
+        if found and var_to_search == 1:
+            functions.display(temp_field_service)
+        elif found:
+            [functions.display(dict_iter) for dict_iter in temp_field_service]
+        else:
+            print(f"{search_value} was not found")
+
+        search_again = input("Would you like to search again (Y/N):").strip().upper()
+
+        if search_again == "Y":
+            pass
+        else:
+            [print(f"Invalid parameter in {search_again}") if search_again != "N" else print("Search protocol terminated")]
+            search_again = False
+
+
+
+def validate_choice():
+    global choice
+    if not functions.check_if_dict_empty(field_service):
+        print("The record is empty so limited options available")
+        choice = input("Add(4), Quit(6): ").strip()
+        if choice in ["1","2","3","5"]:
+            print("ERROR:These numbers cannot be accepted")
+            return False
+        else:
+            return True
+    else:
+        choice = input("Edit (1), View (2), Delete(3), Add(4), Search(5), Quit(6): ").strip()
+        return True
+
+print("This is the client tracker app:")
+print("Use this to keep track of your clients")
+choice = ""
+
+
+
+while choice != "6":
+
+    #Prevents a lot of issues by blocking some options if the record is empty
+    if not validate_choice():
+        continue
+
+    #Add function
+    if choice == "1":
+        main_edit()
+
+    elif choice == "2":
+        functions.display(field_service)
+
+    #Deletion choice
+    elif choice == "3":
+        main_delete()
+
+    elif choice == "4":
+        main_add()
+
+    elif choice == "5":
+        main_search()
+
+    elif choice != "6":
+        print("Please enter a valid number")
+
+
+    functions.dict_to_csv(field_service,DATA_FILE)
+
+print("\nProgram successfully terminated")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
